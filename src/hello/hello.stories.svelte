8821ddf2be60bf9ef2313caca7673105e15dd91c
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2022-03-22 22:00:55 +0300

    Simplify configuration: use the component template
    Signed-off-by: Kaleab S. Melkie <kaleabmelkie@gmail.com>

---
+++ src/hello/hello.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Hello from './hello.svelte'
+</script>
+
+<Meta
+  title="Examples/Hello"
+  component={Hello}
+  argTypes={{
+    name: { control: 'text' },
+  }}
+/>
+
+<Template let:args>
+  <Hello {...args} />
+</Template>
+
+<Story name="world" args={{ name: 'world' }} />
+<Story name="Svelte" args={{ name: 'Svelte' }} />
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as default } from './hello/hello.svelte'
